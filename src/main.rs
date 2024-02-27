5026514df86120824f35cefa3281c32fb8f3cf83
Chris Sergienko <iam@theonewho.codes>
2024-02-27 23:12:27 +0200

    add configuration setup, split into modules, update test to point to postgres, update github actions

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use std::net::TcpListener;
-use zero2prod::run;
+use zero2prod::configuration::get_configuration;
+use zero2prod::startup::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let listener =
-        TcpListener::bind("127.0.0.1:8000").expect("Failed to bind listener to port 8000");
+    // Panic if we can't read configuration
+    let confiuration = get_configuration().expect("Failed to read configuration");
+    let address = format!("127.0.0.1:{}", confiuration.application_port);
+    let listener = TcpListener::bind(address)?;
     run(listener)?.await
 }
