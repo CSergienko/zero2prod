17dba0fd6afcc32e38216a45f9bb0f11650d283f
Chris Sergienko <iam@theonewho.codes>
2024-02-28 00:32:39 +0200

    set up tests to use unique logical databases

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
+use sqlx::{Connection, PgConnection, PgPool};
 use std::net::TcpListener;
-use zero2prod::configuration::get_configuration;
+use zero2prod::configuration::{self, get_configuration};
 use zero2prod::startup::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     // Panic if we can't read configuration
-    let confiuration = get_configuration().expect("Failed to read configuration");
-    let address = format!("127.0.0.1:{}", confiuration.application_port);
+    let configuration = get_configuration().expect("Failed to read configuration");
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres");
+
+    let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
-    run(listener)?.await
+    run(listener, connection_pool)?.await
 }
